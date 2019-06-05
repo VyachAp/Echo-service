de691e33dae0bd0fa1136a948f9afc3ba459f770
Vyacheslav Apasov <apasov.v31@gmail.com>
2019-06-05 17:09:13 +0700

    Updated service to aioftp and aiodocker, started aio-tracking of pipeline

--- Pipeline.py
+++ Pipeline.py
@@ -1,23 +1,30 @@
-import docker
-import ftplib
+import aiodocker
+import aioftp
 import os
 import subprocess
 from aiohttp import web
-from gcloud import storage
+from gcloud import storage, resource_manager
+from pathlib import Path
+import asyncio
+import aiojobs
+from aiojobs.aiohttp import setup, spawn
+from multidict import MultiDict
+from select import select
+import logging
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 host = "localhost"
 ftp_user = "bob"
 ftp_password = "12345"
-folder = '/home/vyachap/PycharmProjects/Echo/Files'
+folder = Path().absolute()
+status = 'IDLE'
 
 routes = web.RouteTableDef()
 
-con = ftplib.FTP(host=host, user=ftp_user, passwd=ftp_password)
 
-@routes.get('/')
-async def handler(request):
-    return web.Response()
+# con = ftplib.FTP(host=host, user=ftp_user, passwd=ftp_password)
+
 
 # Loading block
 # async def upload_files():
@@ -32,54 +39,36 @@ async def handler(request):
 
 
 # Downloading block
-async def download_csv(path: str):
-    filenames = con.nlst()
-    print("Received list of files")
-    for filename in filenames:
-        host_file = os.path.join(path, filename)
-        try:
-            with open(host_file, 'wb') as local_file:
-                con.retrbinary('RETR ' + filename, local_file.write)
-                print("File {} downloaded". format(filename))
-        except ftplib.error_perm:
-            pass
+async def download_csv():
+    global status
+    status = "Files downloading"
+    async with aioftp.ClientSession(host=host, user=ftp_user, password=ftp_password) as client:
+        for path, info in (await client.list()):
+            await client.download(path, destination="./Work/pipeline/" + path.name, write_into=True)
 
 
 async def model_training():
-    print("R script is running...")
-    subprocess.call(['Rscript', 'estimate_models.R', '--booked', 'md.csv', '--cogs', 'mdcogs.csv'])
-    print("R model relearned")
-
-
-@routes.get('/download')
-async def download_endpoint(request):
-    try:
-        download_csv(folder)
-    except Exception as e:
-        return web.Response(text="Download Failed. Reason: {}".format(e), status=500)
-    return web.Response(text="Download Complete", status=200)
-
-
-@routes.get('/training')
-async def training_endpoint(request):
-    try:
-        model_training()
-    except Exception as e:
-        return web.Response(text="R-model training failed. Reason: {}".format(e), status=500)
-    return web.Response(text="R-model trained successfully", status=200)
+    global status
+    status = "Model training."
+    await asyncio.create_subprocess_shell('Rscript /home/vyachap/Work/pipeline/estimate_models.R '
+                                          '--booked /home/vyachap/Work/pipeline/md.csv '
+                                          '--cogs /home/vyachap/Work/pipeline/mdcogs.csv')
 
 
 async def build_image():
-    client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    image = client.build(dockerfile='/home/vyachap/PycharmProjects/Echo/Dockerfile', rm=True)
+    global status
+    status = "Image building."
+    docker = aiodocker.Docker()
+    # Add a real path to Dockerfile in r-project-service.
+    await docker.images.build(path_dockerfile='./r-driveproject/Dockerfile', tag='lol:kek', rm=True)
 
 
-@routes.get('/start_pipeline')
-async def start_pipeline(request):
-    pass
+@routes.get('/status')
+async def status_endpoint(request):
+    global status
+    return web.Response(text=status)
 
 
-@routes.get('/csv_upload')
 async def upload_csv(request):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket("ad_documents")
@@ -87,7 +76,7 @@ async def upload_csv(request):
     blob.upload_from_filename('D:/Download/02-06-53.pdf')
 
 
-async def upload_model():
+async def upload_model(request):
     storage_client = storage.Client()
     bucket = storage_client.get_bucket("ad_documents")
     blob = bucket.blob('/chosen-path-to-object/{name-of-object}')
@@ -97,8 +86,60 @@ async def upload_model():
 async def upload_image():
     pass
 
-# app = web.Application()
-# web.run_app(app)
 
-# build_image()
+@routes.post('/training')
+async def training_endpoint(request):
+    try:
+        await model_training()
+    except Exception as e:
+        return web.Response(text="R-model training failed. Reason: {}".format(e), status=500)
+    return web.Response(text="R-model trained successfully", status=200)
 
+
+@routes.get('/start_pipeline')
+async def start_pipeline(request):
+    scheduler = await aiojobs.create_scheduler()
+    await scheduler.spawn(start())
+    return web.Response(text="Pipeline started")
+
+
+async def start():
+    global status
+    try:
+        await download_csv()
+    except Exception as e:
+        status = 'FAILED'
+        return web.Response(text="Download failed. Reason: {}".format(e), status=500)
+    try:
+        await model_training()
+    except Exception as e:
+        status = 'FAILED'
+        return web.Response(text="R-model training failed. Reason: {}".format(e), status=500)
+    # upload_csv()
+    # upload_model()
+    try:
+        await build_image()
+    except Exception as e:
+        status = 'FAILED'
+        return web.Response(text="Image building failed. Reason: {}".format(e), status=500)
+    # upload_image()
+
+
+async def file_upload_handler(request):
+    data = await request.post()
+    script = data['script']
+    filename = script.filename
+    script_file = data['script'].file
+    content = script_file.read()
+    return web.Response(body=content, headers=MultiDict({'CONTENT-DISPOSITION': script_file}))
+
+
+@routes.get('/')
+async def handler(request):
+    return web.Response(text='OK')
+
+
+app = web.Application()
+app.add_routes(routes)
+setup(app)
+web.run_app(app)
